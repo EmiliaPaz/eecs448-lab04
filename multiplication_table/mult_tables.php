43dc95a751eea7f86c2c4e82f4c364479a86525d
Emilia Paz <emilia.paz@ku.edu>
2018-10-02 00:19:11 -0500

    [update] Implemented multiplication table using html inside php

--- multiplication_table/mult_tables.php
+++ multiplication_table/mult_tables.php
@@ -8,7 +8,33 @@
 	<body>
 
         <table>
-            
+            <!-- First row -->
+            <tr>
+                <th></th>
+        		<?php
+                    for($x = 1; $x <= 100; $x++):
+        				echo "<th> $x </th>";
+        		    endfor;
+                ?>
+            </tr>
+
+            <?php
+                for($x = 1; $x <= 100; $x++):
+                    echo "<tr>";
+                        for($y = 1; $y <= 100; $y++):
+                            // First col
+                            if($y == 0):
+                                echo "<th> $y </th>";
+                            // Multiplication rows and cols
+                            else:
+                                $z = $x * $y;
+                                echo "<td> $z </td>";
+                            endif;
+                        endfor;
+                    echo "</tr>";
+                endfor;
+            ?>
+
         </table>
 
 	</body>
